f94cccca61be14da9126a887472080f08627b3c3
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-03-14 19:57:42 +0530

    app

--- app.js
+++ app.js
@@ -5,10 +5,21 @@
 
 const express = require("express");
 const db = require("./db/mongo");
-
+const pizza = require("./modules/pizzaModule");
 const app = express();
 const PORT = 5000;
 app.use(express.json());
+
+app.get("/getpizzas", (req, res) => {
+  pizza.find({}, (err, docs) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(docs);
+      res.send(docs);
+    }
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`server running at ${PORT}`);
