011d1bf452f4f3ea4e80696a6a73bd7c155526ca
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-03-14 19:08:02 +0530

    mongo

--- db/mongo.js
+++ db/mongo.js
@@ -30,3 +30,15 @@ const mongoose = require("mongoose");
 
 var mongoURL =
   "mongodb+srv://sakthi123:sakthi123@cluster0.ydepc.mongodb.net/pizza?retryWrites=true&w=majority";
+mongoose.connect(mongoURL, { useUnifiedTopology: true, useNewUrlParser: true });
+
+var db = mongoose.connection;
+
+db.on("connected", () => {
+  console.log("mongo db connection successfully");
+});
+db.on("error", () => {
+  console.log("mongo db connection failer");
+});
+
+module.exports = mongoose;
