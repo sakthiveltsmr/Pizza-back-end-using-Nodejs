32586aaf26f95b5244f7d06184913523a98702f7
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-03-14 19:12:29 +0530

    app.js

--- app.js
+++ app.js
@@ -7,29 +7,8 @@ const db = require("./db/mongo");
 
 const app = express();
 const PORT = 5000;
+app.use(express.json());
 
-const users = [
-  {
-    name: "sakthi",
-    age: 23,
-  },
-  {
-    name: "vel",
-    age: 45,
-  },
-];
-(async () => {
-  app.use(express.json()); //parsing data ; string json convert
-
-  await db.connectDB();
-  app.get("/", (req, res) => {
-    res.status(200).send("Server is running sucessfully");
-  });
-  app.get("/users/", (req, res) => {
-    res.status(200).send(users);
-  });
-
-  app.listen(PORT, () => {
-    console.log("server is running at", PORT);
-  });
-})();
+app.listen(PORT, () => {
+  console.log(`server running at ${PORT}`);
+});
