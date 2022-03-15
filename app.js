111e27eee83b1be69f13fe818963a82264d04019
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-03-15 21:58:01 +0530

    app

--- app.js
+++ app.js
@@ -8,7 +8,10 @@ const db = require("./db/mongo");
 const pizza = require("./modules/pizzaModule");
 const app = express();
 const PORT = 5000;
+
 app.use(express.json());
+
+const pizzasRoute = require("./routes/pizzasRoute");
 
 app.use("/", (req, res, next) => {
   res.send("server running");
