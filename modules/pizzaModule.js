9af83e8a01592cf8cf34c19ca6e7fb2154c45da1
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-03-17 16:28:54 +0530

    modules

--- modules/pizzaModule.js
+++ modules/pizzaModule.js
@@ -1,18 +1,19 @@
-const { type } = require("express/lib/response");
 const mongoose = require("mongoose");
 
 const pizzaSchema = mongoose.Schema(
   {
     name: { type: String, require },
-    varient: [],
+    varients: [],
     prices: [],
     category: { type: String, require },
     image: { type: String, require },
     description: { type: String, require },
   },
-  { timestamps: true }
+  {
+    timestamps: true,
+  }
 );
 
-const pizzaModel = mongoose.model("pizza", pizzaSchema);
+const pizzaModel = mongoose.model("pizzas", pizzaSchema);
 
 module.exports = pizzaModel;
