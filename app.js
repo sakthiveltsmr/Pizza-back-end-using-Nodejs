aafdbf7c49ac96287b3a8f19d35c2f6d2233a131
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-03-17 16:48:07 +0530

    app

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const express = require("express");
 const db = require("./db/mongo");
-const pizza = require("./modules/pizzaModule");
 const app = express();
 const PORT = 5000;
 
 app.use(express.json());
 
 const pizzasRoute = require("./routes/pizzasRoute");
+const path = require("path");
+const pizzasRoute = require("./routes/pizzasRoute");
+const userRoute = require("./routes/userRoute");
+const ordersRoute = require("./routes/ordersRoute");
 
-app.use("/api/pizzas", pizzasRoute);
+app.use("/api/pizzas/", pizzasRoute);
+app.use("/api/users/", userRoute);
+app.use("/api/orders/", ordersRoute);
+
+// app.use("/api/pizzas", pizzasRoute);
 
 app.use("/", (req, res, next) => {
   res.send("server running");
