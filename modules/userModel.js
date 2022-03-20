fdbd729fdddb00e69053d4d4718bb70a33eeb62f
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-03-20 22:13:59 +0530

    usermodule

--- modules/userModel.js
+++ modules/userModel.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose");
 
-const userSchema = mongoose.Schema({
-    name : {type: String , require},
-    email : {type: String , require},
-    password : {type: String , require},
-    isAdmin : {type: Boolean , require , default: false},
-} , {
-    timestamps : true,
-})
+const userSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    isAdmin: { type: Boolean, required: true, default: false },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-module.exports = mongoose.model('users' , userSchema)
+module.exports = mongoose.model("users", userSchema);
