81430dc5cb1f1c62356058b6e659df4478aee7f9
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-03-22 20:49:55 +0530

    user

--- modules/userModel.js
+++ modules/userModel.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema(
   {
-    name: { type: String, required: true },
-    email: { type: String, required: true },
-    password: { type: String, required: true },
-    isAdmin: { type: Boolean, required: true, default: false },
+    name: { type: String, require },
+    email: { type: String, require },
+    password: { type: String, require },
+    isAdmin: { type: Boolean, require, default: false },
   },
   {
     timestamps: true,
