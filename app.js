ad95e64a8c1345fbad17d77fa951f071243864f6
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-03-25 23:09:46 +0530

    app

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const db = require("./db/mongo");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(express.json());
 
 const path = require("path");
