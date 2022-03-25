b38fae8e8373888b1aabf543a4a05b5e5dfc2bf4
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-03-25 21:41:47 +0530

    app

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const db = require("./db/mongo");
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
